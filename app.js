391273b580105abe7ac532d8c3001ed66ed881d3
Slava Knyazev <slava@knyz.org>
2016-07-28 19:51:53 -0400

    get over with

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var validUrl = require('valid-url');
+var Crypto = require('crypto');
+
+var urls = [];
+
+function isValidUrl(url){
+  if (validUrl.isUri(url)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Taken from https://github.com/viruzx/Collectivist/blob/master/app.js
+function newToken() {
+    return Crypto.randomBytes(8).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
+}
+
+function shorten(url){
+  var short =  newToken();
+  urls[short] = url;
+  return short;
+}
+app.get('/', function (req, res) {
+  res.send("Usage here: https://little-url.herokuapp.com/");
+});
+app.get('/:short', function (req, res) {
+
+  if (urls[req.params.short]){
+    res.redirect(urls[req.params.short]);
+    res.send("Redirecting to " + urls[req.parmas.short]);
+  } else {
+    var response = {
+      error: "URL not found"
+    }
+    res.send(JSON.stringify(response));
+  }
+
+});
+app.get('/new/*', function (req, res) {
+  var url = req.originalUrl.replace("/new/", "");
+  if (isValidUrl(url)){
+    var short = shorten(url);
+    var response = {
+      short: 'https://localhost:3643/' + short,
+      long:  url
+    }
+    res.send(JSON.stringify(response));
+  } else {
+    var response = {
+      error: "URL not valid"
+    }
+    res.send(JSON.stringify(response));
+  }
+});
+
+app.listen(process.env.PORT || 3543, function () {
+});
